3f3cecc91c5fd6446b11538828563ae456bd6746
Mariam Sherif <marioma03@gmail.com>
2022-11-17 15:19:51 +0200

    map additions and initializations

--- src/game.cpp
+++ src/game.cpp
@@ -31,8 +31,11 @@ class Champion{
 	
 	public:
 		Champion(){
-			loc = Point();
 			cout<<"Constructor champion() is called"<<endl;
+			loc = Point(); // will we alloc since Point is an object or ?
+			loc.setPoint(0,0); // i initialized the origin here tho m not sure if this the right place
+			hp = 100;
+			gemScore = 0;
 		}
 		void print_champ_info(){
 			cout<<"Current hp: "<<hp<<"  , Score: "<<gemScore << " Location: " <<loc.toString()<<endl;
@@ -49,12 +52,29 @@ class Map{
 	public:
 		Map(){
 			cout<<"Constructor Map() is called"<<endl;
+			randomize_map();
+			print_map();
 		}
 		void print_map(){
 			cout<<"print_map() called"<<endl;
+			for(int i=0; i<10; i++){
+				for(int j=0; j<10; j++){
+
+					// if(loc.getY==j && loc.getX()==i) // how can i reference the champ's point location hena i googled it it went on abt dynamic mem alloc w keda or maybe theres a diff way google the sentence i sent on discord
+					// cout<<"O"<<" ";
+					// else
+					cout<<"E"<< " ";
+
+				
+
+
+				}
+				cout<<endl;
+			}
 		}
 		void randomize_map(){
 			cout<<"randomize_map() called"<<endl;
+			//randomly waza3 gems and obstacles on the map
 		}
 
 };
@@ -63,7 +83,7 @@ class Map{
 
 
 int main()
-{	char c = (char) 196;
-	cout<<c<<endl;
+{	
+	Map();
 	return 0;
 }
