53baf500d8922f8090eaa1e04a1d777ba6c5456d
AboElWa7awe7 <mohamedelwa7a@gmail.com>
2022-11-18 22:02:59 +0200

    we added a champion mario in the map but no work

--- src/game.cpp
+++ src/game.cpp
@@ -1,89 +1,105 @@
 #include <iostream>
 using namespace std;
 
-class Point{
-	private:
-		int X, Y;
-	public:
-		void setPoint(int a, int b){
-			X = a;
-			Y = b;
-		}
-		int getX(){
-			return X;
-		}
-		int getY(){
-			return Y;
-		}
-		string toString(){
-			string s;
-			s = getX() + ", " + getY();
-			return s;
-		}
+class Point
+{
+private:
+	int X, Y;
+
+public:
+	void setPoint(int a, int b)
+	{
+		X = a;
+		Y = b;
+	}
+	int getX()
+	{
+		return X;
+	}
+	int getY()
+	{
+		return Y;
+	}
+	string toString()
+	{
+		string s;
+		s = getX() + ", " + getY();
+		return s;
+	}
 };
 
-class Champion{
-	private:
-		int hp;
-		int gemScore;
-		Point loc;
-		
+class Champion
+{
+private:
+	int hp;
+	int gemScore;
+	Point loc;
 	
-	public:
-		Champion(){
-			cout<<"Constructor champion() is called"<<endl;
-			loc = Point(); // will we alloc since Point is an object or ?
-			loc.setPoint(0,0); // i initialized the origin here tho m not sure if this the right place
-			hp = 100;
-			gemScore = 0;
-		}
-		void print_champ_info(){
-			cout<<"Current hp: "<<hp<<"  , Score: "<<gemScore << " Location: " <<loc.toString()<<endl;
-		}
-
-
+public:
+	Champion()
+	{
+		cout << "Constructor champion() is called" << endl;
+		loc = Point();		// will we alloc since Point is an object or ?
+		loc.setPoint(0, 0); // i initialized the origin here tho m not sure if this the right place
+		hp = 100;
+		gemScore = 0;
+	}
+	void print_champ_info()
+	{
+		cout << "Current hp: " << hp << "  , Score: " << gemScore << " Location: " << loc.toString() << endl;
+	}
+	
 };
 
-class Map{
-	private:
-		int board[10][10];
+class Map
+{
+private:
+	int board[10][10];
 
+public:
 
-	public:
-		Map(){
-			cout<<"Constructor Map() is called"<<endl;
-			randomize_map();
-			print_map();
-		}
-		void print_map(){
-			cout<<"print_map() called"<<endl;
-			for(int i=0; i<10; i++){
-				for(int j=0; j<10; j++){
+	Map()
+	{
+		
+		Champion mario;
+		cout << "Constructor Map() is called" << endl;
+		randomize_map();
+		print_map();
+	}
+	int getChampX(){
+		return mario.loc.getX();
+	}
+	int getChampY(){
+		return mario.loc.getX();
+	}
+	
+	void print_map()
+	{
+		cout << "print_map() called" << endl;
+		for (int i = 0; i < 10; i++)
+		{
+			for (int j = 0; j < 10; j++)
+			{
 
-					// if(loc.getY==j && loc.getX()==i) // how can i reference the champ's point location hena i googled it it went on abt dynamic mem alloc w keda or maybe theres a diff way google the sentence i sent on discord
-					// cout<<"O"<<" ";
-					// else
-					cout<<"E"<< " ";
-
-				
-
-
-				}
-				cout<<endl;
+				// if(loc.getY==j && loc.getX()==i) // how can i reference the champ's point location hena i googled it it went on abt dynamic mem alloc w keda or maybe theres a diff way google the sentence i sent on discord
+				// cout<<"O"<<" ";
+				// else
+				cout << "E"
+					 << " ";
 			}
+			cout << endl;
 		}
-		void randomize_map(){
-			cout<<"randomize_map() called"<<endl;
-			//randomly waza3 gems and obstacles on the map
-		}
-
+	}
+	void randomize_map()
+	{
+		cout << "randomize_map() called" << endl;
+		// randomly waza3 gems and obstacles on the map
+	}
 };
 
-
-
-//GRABY TEST LEARNER PERMITS
+// GRABY TEST LEARNER PERMITS HAHA GAY WAMEN grayb
 int main()
-{	
+{
 	Map();
 	return 0;
 }
