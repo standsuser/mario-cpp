4b2c493eb714b62d51cdc5d02a7d3d6ca15c3553
Rambo <smido964@gmail.com>
2022-11-20 20:57:23 +0200

    newGame() fix, 2d array remains

--- src/game.cpp
+++ src/game.cpp
@@ -85,11 +85,12 @@ private:
 public:
 	Map()
 	{	
-		randomize_map();	
+		randomize_map();
 		if(flag == 0){
-		cout << "Constructor Map() is called" << endl;
-		cout << "randomize_map() called" << endl;
-	}flag++;
+			cout << "Constructor Map() is called" << endl;
+			cout << "randomize_map() called" << endl;
+		}
+		flag++;
 	}
 	void end(){
 		if(mario->getScore() == gemCount){
@@ -110,7 +111,7 @@ public:
 	void newTurn()
 	{
 		// clear terminal
-
+		
 		cout << "\033[2J\033[1;1H";
 		print_map();
 		cout << "Total Gem count: " << gemCount << "\nObst count: " << obstCount << "\nScore: " << mario->getScore() << "\nHealth: " << mario->getHp() << "\nx: " << mario->getX() << "\ny: " << mario->getY() << endl;
@@ -268,11 +269,9 @@ public:
 	}
 
 	void newGame(){
-		cout << "\033[2J\033[1;1H";
-		print_map();
-		cout<<"Choose 1 to Randomize, Choose 2 to start the game"<<endl;
-		char rButton = getch();
 		
+		print_map();
+		char rButton = 0;
 	
 		while (rButton != '1' && rButton != '2'){
 			cout<<"Choose 1 to Randomize again, Choose 2 to start the game"<<endl;
@@ -280,8 +279,9 @@ public:
 		}
 		
 		if(rButton == '1'){
-			cout << "\033[2J\033[1;1H";
+			//cout << "\033[2J\033[1;1H";
 			randomize_map();
+			cout << "\033[2J\033[1;1H";
 			newGame();
 			
 		}else if(rButton == '2') {
@@ -293,7 +293,8 @@ public:
 
 	void print_map()
 	{
-		int count = 0;
+		int count = 0;	
+		cout<<"print_map() called"<<endl;
 		
 		for (int i = 9; i >= 0; i--)
 		{
@@ -379,7 +380,7 @@ public:
 
 int main()
 {
-	 Map* m = new Map();
+	Map* m = new Map();
 	m->newGame();
 // cout<<"WAAAAAA";
 // 	int x;
